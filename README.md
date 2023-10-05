dcb530d93103785fcf4e3b92c626cb68dd400fd3
GIUSEPPE-ESTUDIANTE <giusepe002@gmail.com>
2023-10-05 15:03:39 -0300

    teste

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # aulas-javascript
- conhecendo 
+
+ ## Sobre a liguagem
+
+ - javaScript  NÂO È Java!
+ - JavaScript pode ser abreviado como JS
+ - È uma liguagem voltada principalmente para o Front-End
+ - Mas tambem pode ser usado Back-end atraves do Node.js
+ - JS pode ser usado para criar recursos interativos no site,
+ como por exemplo:
+   - Galeria de fotos
+   - Carounsel/Slider de conteudo
+   - Funcionalidades de navegaçâo
+   - Validaçôes mais Avançadas de formulario
+- JS pode ser usado ate mesmo para criaçâo de aplicativos e games
---
+++ js/01-sintaxe.js
@@ -0,0 +1,34 @@
+console.log("Programador web")
+console.log("senac penha")
+
+/* Comentario - ALT SHIFT A */
+// Comentario - CTRL
+
+
+
+/* Manipulando dados  */
+
+// Variaveis
+let curso = "HTML5 E CSS3";
+let ano = 2023;
+
+// Constantes 
+
+const mediaMinima = 7;
+
+
+// Saida de dados a partir da memoria 
+console.log(curso)
+console.log(mediaMinima)
+console.log(ano)
+
+ /*   Estou fazendo o curso de HTML5 e CSS3
+ Nestw ano de 2023*/
+
+// Template String/Literal: saida usando crases de ${}
+
+console.log(`Estou fazendo o curso de ${curso}neste ano de ${ano}`);
+
+// Concatenaçâo
+
+console.log("Estou fazendo o curso de " +curso+"neste ano de "+ano+"." )

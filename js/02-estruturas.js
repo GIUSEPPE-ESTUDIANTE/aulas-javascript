210f19f71fa8d4cc03e1d1bb3e7d1cb493c8b85a
GIUSEPPE-ESTUDIANTE <giusepe002@gmail.com>
2023-10-11 13:37:28 -0300

    10-10

--- js/02-estruturas.js
+++ js/02-estruturas.js
@@ -1,66 +1,112 @@
-/* Estruturas de Dados  
- -Arrays (vetores/matrizes):
- lista de dados indexados, sequeciais e acessiveis por um indice numerico.
+/* Estruturas de Dados 
 
- -objetos
- lista de dados nao indexados e formados por propriedades 
- e valores*/
-//  Arrays
+- Arrays (Vetores/Matrizes): 
+Lista de dados indexados, sequenciais e acessíveis por um
+índice numérico.
 
-let cursos = ["node.js", "React", "SQL", "UX/UI Design"]
+- Objetos
+Lista de dados não indexados e formados por propriedades
+e valores. */
 
-console.log(cursos); // saida direita/estrurada
+// Arrays (os índices sempre começam no zero)
+let cursos = ["Node.js", "React", "SQL", "UX/UI Design"];
 
-console.log(cursos[2]); // SQL 
+console.log(cursos); // saída direta/estruturada (APENAS NO console)
+console.log(cursos[2]); // SQL
 
-const dados = ["Giuseppe", "costa", "19", "(11)94296-2546", "Sâo Paulo"]
+/* Exercícios
+1) Crie uma constante chamada "dados" e coloque nela
+os dados de uma pessoa: nome, sobrenome, idade, telefone e cidade */
+const dados = ["Fulano", "Silva", 25, "11-2135-0300", "São Paulo"];
 
-console.log(dados[0]);
-
-console.log(dados[2]);
-
+/* 2) Mostre no console uma mensagem contendo somente o nome e a idade
+da pessoa. Exemplo: "Fulano tem 25 anos." */
+console.log(dados);
 console.log(`${dados[0]} tem ${dados[2]} anos.`);
 
-
-console.log("---------");
+console.log("----------");
 
 // Objeto
-
 let carro = {
-    marca: "fiat",
-    modelo: "uno com escada",
+    marca: "Fiat",
+    modelo: "Uno com escada",
     portas: 2,
     cor: "branco",
-    opcionais: ["Ar condicionado ", "vidros eletricos", "Alarme"]
-
-}
+    opcionais: ["Ar condicionado", "Vidros elétricos", "Alarme"]
+};
 
 console.log(carro);
 console.log(carro.modelo);
-
-
-console.log(
-    `carros da ${carro.marca} tem a cor ${carro.cor}`
-)
-
+console.log(`Carros da ${carro.marca} tem a cor ${carro.cor}`);
 console.log(`Este carro tem ${carro.opcionais[0]} e ${carro.opcionais[2]}`);
 
+/* Exercícios
+1) Crie um novo objeto contendo informações de algo que você gosta
+(filme, livro, comida, jogo etc) */
+let livro = {
+    titulo: "O Senhor dos Anéis",
+    autor: "J.R.R. Tolkien",
+    volumes: 3,
+    genero: "Aventura"
+};
+
+/* 2) Mostre uma mensagem com pelo menos 2 propriedades do objeto
+que você criou. */
+console.log(`O universo ${livro.titulo} foi criado por ${livro.autor}`);
+
+console.log("----------------");
+
+// Objeto com array e com outro objeto
+let pessoa = {
+    nome: "Shiryu",
+    idade: 20,
+    telefones: ["11-2135-0300", "11-91234-5678"],
+    medidas: {
+        peso: 65,
+        altura: 1.75
+    }
+};
+
+console.log(`Nome: ${pessoa.nome}`);
+console.log(`Celular: ${pessoa.telefones[1]}`);
+console.log(`Peso: ${pessoa.medidas.peso} kg `);
+
+// Array de objetos
+let alunos = [
+    {
+        nome: "Naruto",
+        idade: 10
+    },
+    {
+        nome: "Guts",
+        idade: 30
+    },
+    {
+        nome: "Dohko",
+        idade: 120
+    }
+];
+
+console.log( alunos[1].nome ); // Guts
+console.log( alunos[0].nome ); // Naruto
+console.log(
+`O cavaleiro de ouro originalmente era o ${alunos[2].nome} de Libra que tem ${alunos[2].idade} anos`);
+
+
+// Array como matriz
+let tecnologias = [
+    ["HTML5", "CSS3", "JavaScript"],
+    ["PHP", "SQL", "APIs", "Node", "Python"],
+    ["Figma", "Photoshop"]
+];
+
+console.log(tecnologias[1][3]); // Node
+console.log(tecnologias[2][1]); // Photoshop
+console.log(tecnologias[0][0]); // HTML5
 
 
 
-let game = {
 
-    nome: "Rocket League",
-    lançamento: "2015",
-    modo: "Futebol de carro",
-    downlad: "20GB"
-
-}
-
-console.log(game);
-console.log(game.nome);
-
-console.log(`jogo gratis pra abaixar ${game.downlad}`);
 
 
 

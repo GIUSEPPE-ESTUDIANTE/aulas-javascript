2b381167ebecb80ff00b3759d8c89862d82ff4dd
GIUSEPPE-ESTUDIANTE <giusepe002@gmail.com>
2023-10-18 17:02:01 -0300

    18-10

--- js/07-dom-eventos.js
+++ js/07-dom-eventos.js
@@ -16,10 +16,26 @@ Seleciona VÀRIOS elementos atraves de um/varios seletor(es)
 
 /* Exemplo */
 
+// 
 const subtitulo = document.getElementById("subtitulo-dom");
 const desemho = document.querySelector(".exemplo");
 const descricao = document.querySelector("#descricao")
 
 console.log(desemho);
 console.log(descricao);
+
+const elementos = document.querySelectorAll("h3, a");
+console.log(elementos);
+console.log(elementos[1]);
+
+// Modificando o DOM
+const titulo = document.querySelector("h1");
+titulo.textContent = "Ola JavaScript";
+titulo.style.textAlign = "center"; // CSS inline via JS
+titulo.classList.add("destaque"); // classe CSS via JS
+
+const linksReferencia = document.querySelectorAll("ul li a");
+console.log(linksReferencia);
+
+let quantidadelinks = linksReferencia.length;
 

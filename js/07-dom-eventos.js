c58ac9c1b0e87e79b6b4f584ef27269514c8af76
GIUSEPPE-ESTUDIANTE <giusepe002@gmail.com>
2023-10-18 16:19:22 -0300

    18-10

---
+++ js/07-dom-eventos.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/* Funçôes de seleçâo no DOM
+
+-getElementyById()
+Seleciona UM elemento atraves de um id
+
+-querySelector()
+Seleciona UM elemento através de um seletor.
+
+-querySelectorALL()
+Seleciona VÀRIOS elementos atraves de um/varios seletor(es)
+
+*/
+
+
+/* Exemplo */
+
+const subtitulo = document.getElementById("subtitulo-dom");
+const desemho = document.querySelector(".exemplo");
+const descricao = document.querySelector("#descricao")
+
+console.log(desemho);
+console.log(descricao);
+
